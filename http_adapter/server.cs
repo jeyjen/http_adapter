e320f432a313d89d913900f556fd2d24a762a567
eugene <novikov.eugeniy@gmail.com>
2017-03-21 07:53:40 +0400

    реализована доставка содержимого swagger ui

--- http_adapter/server.cs
+++ http_adapter/server.cs
@@ -7,7 +7,6 @@ using jeyjen.extension;
 using Apache.NMS;
 using Apache.NMS.Util;
 using System.IO;
-using http_adapter.provider;
 
 namespace http_adapter
 {
@@ -82,95 +81,137 @@ namespace http_adapter
         protected override void on_http_request(context context)
         {
             // не обрабатывать запрос на полученеи favicon
-            if (context.request.url.Contains("favicon")) 
+            var ext = Path.GetExtension(context.request.url).ToLower();
+            if (!ext.is_null_or_empty() || context.request.url.is_equals("/"))
             {
-                context.responce.status = status.Bad_Request;
-                context.responce.send();
-                return;
-            }
+                // вернуть документацию
+                var fn = context.request.url.is_equals("/") ? "index.html" : context.request.url.Substring(1);
+                fn = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "src", fn);
 
-
-            var req = context.request;
-            var parts = req.url.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
-            
-            string prefix = "";
-            string performer = "";
-            string ctx = "";
-            string action = "";
-            if (parts.Length > 0)
-            {
-                int offset = 0;
-                if (prefixes.Contains(parts[0].ToLower()))
+                var fi = new FileInfo(fn);
+                if (File.Exists(fn))
                 {
-                    offset = 1;
-                    prefix = parts[0].ToLower();
-                }
-                if (parts.Length - offset > 0)
-                {
-                    performer = parts[offset].ToLower();
-                }
-                if (parts.Length - offset > 1)
-                {
-                    ctx = parts[offset + 1].ToLower();
-                }
-                if (parts.Length - offset > 2)
-                {
-                    action = parts[offset + 2].ToLower();
-                }
-                if (performers.Contains(performer) || performers.Contains(performer + ".info"))
-                {
-                    var qn = new StringBuilder();
-                    if (!prefix.is_null_or_empty())
+                    var bytes = File.ReadAllBytes(fn);
+                    switch (ext)
                     {
-                        qn.AppendFormat("{0}.", prefix);
-                    }
-                    qn.AppendFormat("@{0}", performer);
-                    var prod = _session.CreateProducer(SessionUtil.GetDestination(_session, qn.ToString()));
-                    var m = _session.CreateBytesMessage();
-                    m.Properties["context"] = ctx;
-                    m.Properties["action"] = action;
-                    
-                    if (context.request.headers.ContainsKey("Content-Type"))
-                    {
-                        m.Properties["content-type"] = context.request.headers["Content-Type"];
-                    }
-                    byte[] data = null;
-                    if (context.request.method.is_equals("GET"))
-                    {
-                        if (context.request.url_param.Count > 0)
-                        {
-                            var c = new flex();
-                            foreach (var p in context.request.url_param)
+                        case ".html":
                             {
-                                c.set(p.Key, p.Value);
+                                context.responce.content_type = "text/html";
                             }
-                            data = Encoding.UTF8.GetBytes(c.json());
-                        }
+                            break;
+                        case ".js":
+                            {
+                                context.responce.content_type = "text/javascript";
+                            }
+                            break;
+                        case ".css":
+                            {
+                                context.responce.content_type = "text/css";
+                            }
+                            break;
+                        case ".gif":
+                            {
+                                context.responce.content_type = "image/gif";
+                            } break;
+                        case ".png":
+                            {
+                                context.responce.content_type = "image/png";
+                            }
+                            break;
+                            
                     }
-                    else
-                    {
-                        data = context.request.body;
-                    }
-                    m.Content = data;
-                    var rq = SessionUtil.GetDestination(_session, null, DestinationType.TemporaryQueue);
-                    var consumer = _session.CreateConsumer(rq);
-                    consumer.Listener += on_responce;
-
-                    var id = guid.generate(guid_type.at_end).ToString();
-                    m.NMSCorrelationID = id;
-                    _request.Add(id, new Tuple<context, IMessageConsumer>(context, consumer));
-                    prod.Send(m);
+                    context.responce.send(bytes);
                 }
                 else
                 {
                     context.responce.status = status.Bad_Request;
-                    context.responce.send("обработчик \"{0}\" не определен".format(performer));
+                    context.responce.send();
                 }
             }
             else
             {
-                context.responce.status = status.Bad_Request;
-                context.responce.send("некорректный url");
+                var req = context.request;
+                var parts = req.url.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string prefix = "";
+                string performer = "";
+                string ctx = "";
+                string action = "";
+                if (parts.Length > 0)
+                {
+                    int offset = 0;
+                    if (prefixes.Contains(parts[0].ToLower()))
+                    {
+                        offset = 1;
+                        prefix = parts[0].ToLower();
+                    }
+                    if (parts.Length - offset > 0)
+                    {
+                        performer = parts[offset].ToLower();
+                    }
+                    if (parts.Length - offset > 1)
+                    {
+                        ctx = parts[offset + 1].ToLower();
+                    }
+                    if (parts.Length - offset > 2)
+                    {
+                        action = parts[offset + 2].ToLower();
+                    }
+                    if (performers.Contains(performer) || performers.Contains(performer + ".info"))
+                    {
+                        var qn = new StringBuilder();
+                        if (!prefix.is_null_or_empty())
+                        {
+                            qn.AppendFormat("{0}.", prefix);
+                        }
+                        qn.AppendFormat("@{0}", performer);
+                        var prod = _session.CreateProducer(SessionUtil.GetDestination(_session, qn.ToString()));
+                        var m = _session.CreateBytesMessage();
+                        m.Properties["context"] = ctx;
+                        m.Properties["action"] = action;
+
+                        if (context.request.headers.ContainsKey("Content-Type"))
+                        {
+                            m.Properties["content-type"] = context.request.headers["Content-Type"];
+                        }
+                        byte[] data = null;
+                        if (context.request.method.is_equals("GET"))
+                        {
+                            if (context.request.url_param.Count > 0)
+                            {
+                                var c = new flex();
+                                foreach (var p in context.request.url_param)
+                                {
+                                    c.set(p.Key, p.Value);
+                                }
+                                data = Encoding.UTF8.GetBytes(c.json());
+                            }
+                        }
+                        else
+                        {
+                            data = context.request.body;
+                        }
+                        m.Content = data;
+                        var rq = SessionUtil.GetDestination(_session, null, DestinationType.TemporaryQueue);
+                        var consumer = _session.CreateConsumer(rq);
+                        consumer.Listener += on_responce;
+
+                        var id = guid.generate(guid_type.at_end).ToString();
+                        m.NMSCorrelationID = id;
+                        _request.Add(id, new Tuple<context, IMessageConsumer>(context, consumer));
+                        prod.Send(m);
+                    }
+                    else
+                    {
+                        context.responce.status = status.Bad_Request;
+                        context.responce.send("обработчик \"{0}\" не определен".format(performer));
+                    }
+                }
+                else
+                {
+                    context.responce.status = status.Bad_Request;
+                    context.responce.send("некорректный url");
+                }
             }
         }
         private void on_responce(IMessage message)
